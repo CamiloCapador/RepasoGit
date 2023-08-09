58b57c8717d5e56530ba45d82738031b09b22e35
CamiloCapador <juanka1307@gmail.com>
2023-08-09 18:33:33 -0500

    Cuarta configuracion

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main(args: Array<String>) {
     println("Hello World!")
     println("Segunda salida por consola Kotlin")
+    println("Tercer salida por consola")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
